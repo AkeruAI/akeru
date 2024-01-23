d0640ec643818736436c679763f67325e643b036
Alejandro <smarketing.cuz@gmail.com>
2024-01-23 15:42:35 -0500

    Add dotenv package and import it in gpt4Adapter.ts

--- src/infrastructure/adaptaters/gpt4Adapter.ts
+++ src/infrastructure/adaptaters/gpt4Adapter.ts
@@ -1,6 +1,7 @@
 import OpenAI from "openai";
 import type { ChatCompletionChunk } from "openai/resources";
 import type { Stream } from "openai/streaming";
+import "dotenv/config";
 
 const openai = new OpenAI({
   apiKey: process.env.OPENAI_API_KEY,
