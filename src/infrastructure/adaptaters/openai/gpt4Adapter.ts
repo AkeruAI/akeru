ea3bc7d629af44d4c1b7fb1d5b87698e01fc743c
Alejandro <smarketing.cuz@gmail.com>
2024-02-01 15:01:09 -0500

    Add GPT-4 Adapter and Test

---
+++ src/infrastructure/adaptaters/openai/gpt4Adapter.ts
@@ -0,0 +1,33 @@
+export async function gpt4Adapter(
+  messages: string,
+  SYSTEM_PROMPT: string
+): Promise<unknown> {
+  try {
+    const res = await fetch("https://api.openai.com/v1/chat/completions", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${process.env.OPENAI_API_KEY}`,
+      },
+      body: JSON.stringify({
+        model: "gpt-4",
+        messages: [
+          {
+            role: "system",
+            content: SYSTEM_PROMPT,
+          },
+          {
+            role: "user",
+            content: messages,
+          },
+        ],
+      }),
+    });
+
+    const data = await res.json();
+    return data;
+  } catch (error) {
+    console.log(error);
+    return new Response("Error", { status: 500 });
+  }
+}
