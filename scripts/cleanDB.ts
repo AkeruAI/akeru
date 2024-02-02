7269d1baa40c2fbc8ee81f58462b5c9f07210f3b
GuiBibeau <g.bibeaulaviolette@gmail.com>
2024-02-02 15:22:44 -0500

    remove neo4j

--- scripts/cleanDB.ts
+++ scripts/cleanDB.ts
@@ -1,13 +1,9 @@
-import { getNeo4jSession } from "@/infrastructure/adaptaters/neo4jAdapter";
 import { redis } from "@/infrastructure/adaptaters/redisAdapter";
 
 const script = async () => {
   try {
-    const session = getNeo4jSession();
     await redis.flushdb();
     console.log("cleaned redis db");
-    await session.run("MATCH (n) DETACH DELETE n");
-    console.log("cleaned neo4j db");
     console.log("done");
     process.exit();
   } catch (e) {
