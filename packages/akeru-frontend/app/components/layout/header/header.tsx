51fa62ef6f4068ad7d86353014373fcad208f528
tobySolutions <adedejitobiloba7@gmail.com>
2024-04-12 15:05:57 +0100

    fix: add cursor-pointer

--- packages/akeru-frontend/app/components/layout/header/header.tsx
+++ packages/akeru-frontend/app/components/layout/header/header.tsx
@@ -6,19 +6,19 @@ function Header() {
     <header>
       <nav className="w-[80%] my-[0] mx-[auto] p-[20px] flex justify-between items-center">
         <div className="w-[100%]">
-          <h1 className="akeru-logo font-[800] text-[30px] tracking-wider">
+          <h1 className="akeru-logo font-[800] text-[30px] tracking-wider cursor-pointer">
             <a href="#">AKERU</a>
           </h1>
         </div>
         <div className="">
           <ul className="flex gap-[25px] items-center">
             {headerRoutes.map((route) => (
-              <li className="text-[12px]" key={route.href}>
+              <li className="text-[12px] cursor-pointer" key={route.href}>
                 {route.text}
               </li>
             ))}
             <div className="h-[21px] w-[1px] bg-white" />
-            <FaGithub className="w-[20.56px] h-[20px]" />
+            <FaGithub className="w-[20.56px] h-[20px] cursor-pointer" />
           </ul>
         </div>
       </nav>
