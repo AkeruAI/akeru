cd2fada29fccc900045f04c2ad82bfea3822d51e
Alejandro <smarketing.cuz@gmail.com>
2024-04-20 15:43:20 -0500

    Add animation styles for text in tailwind.config.ts

--- packages/akeru-frontend/tailwind.config.ts
+++ packages/akeru-frontend/tailwind.config.ts
@@ -8,6 +8,21 @@ const config: Config = {
   ],
   theme: {
     extend: {
+      animation: {
+        text: "text 5s ease infinite",
+      },
+      keyframes: {
+        text: {
+          "0%, 100%": {
+            "background-size": "200% 200%",
+            "background-position": "left center",
+          },
+          "50%": {
+            "background-size": "200% 200%",
+            "background-position": "right center",
+          },
+        },
+      },
       colors: {
         textHover: "#3dff8b",
         buttonBg: "#074707",
