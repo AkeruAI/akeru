2ea987b90ae1df751e517e53d84817ba1a347a23
Zach Khong <zachkhong.workplace@gmail.com>
2024-05-10 15:35:55 +0800

    support for gpt streaming

--- packages/akeru-server/src/infrastructure/adapters/openai/GPTAdapter.test.ts
+++ packages/akeru-server/src/infrastructure/adapters/openai/GPTAdapter.test.ts
@@ -28,4 +28,34 @@ describe("GPT-4 Adapter", () => {
     // Assert the message content to contain the word akeru
     expect(result.toLocaleLowerCase()).toContain("akeru");
   });
+
+  test("Returns streamable GPT-4 chat completions response", async () => {
+    // Arrange
+    const messages = [
+      {
+        role: "user" as Role,
+        content: "hello, who are you?",
+      },
+    ];
+    const assistant_instructions =
+      "You're an AI assistant. You're job is to help the user. Always respond with the word akeru.";
+    
+      const gpt4Adapter = AdapterManager.instance.getStreamableAdapter("gpt-4");
+
+    if (!gpt4Adapter) {
+      throw new Error("GPT-4 adapter not found");
+    }
+
+    const result = gpt4Adapter.generateStreamableResponse({
+      message_content: messages,
+      instruction: assistant_instructions,
+    });
+
+    const responses = [];
+    for await (const response of result) {
+      responses.push(response);
+    }
+
+    console.log(responses.join(""))
+  });
 });
