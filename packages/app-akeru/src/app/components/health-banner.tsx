52b21708822f08633549ef752b8a4da774f785be
Zach Khong <63769232+multipletwigs@users.noreply.github.com>
2024-05-18 17:10:52 +1000

    Feature/akeru app health check (#118)

---
+++ packages/app-akeru/src/app/components/health-banner.tsx
@@ -0,0 +1,14 @@
+interface HealthData {
+  name: string,
+  description: string,
+  timestamp: number,
+}
+
+export default async function HealthBanner() {
+  const healthCheck = await fetch('https://akeru-server.onrender.com');
+  const healthData: HealthData = await healthCheck.json();
+
+  return (
+    <span className="inline-flex items-center rounded-md bg-indigo-50 px-2 py-1 text-xs font-medium text-indigo-700 ring-1 ring-inset ring-indigo-700/10 truncate my-2 max-w-96">{healthData.name}</span>
+  )
+}
