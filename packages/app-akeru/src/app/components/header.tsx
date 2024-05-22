248dfa3187270848e1fa8949ea2b801b932d73b7
Zach Khong <63769232+multipletwigs@users.noreply.github.com>
2024-05-22 15:22:59 +1000

    Chore/app init (#119)

---
+++ packages/app-akeru/src/app/components/header.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <header className="h-16 border-b-grey-300 border-b">
+      <h1>Akeru UI</h1>
+    </header>
+  )
+}
+
+export default Header
