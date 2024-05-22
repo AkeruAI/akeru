97941c29e378720202d32e13f445b9679c5b8d43
Zach Khong <zachkhong.workplace@gmail.com>
2024-05-22 15:22:02 +1000

    added in next projects

---
+++ packages/app-akeru/src/app/(dashboard)/layout.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import Header from '../components/header'
+
+const DashboardLayout = () => {
+  return (
+    <Header></Header>
+  )
+}
+
+export default DashboardLayout
---
+++ packages/app-akeru/src/app/(dashboard)/threads/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const ThreadsPage = () => {
+  return (
+    <div>ThreadsPage</div>
+  )
+}
+
+export default ThreadsPage
